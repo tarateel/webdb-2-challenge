ab4834f75faadd001339e9660fab525a10f96fdf
Tara Sherman <taradelin@gmail.com>
2019-12-25 09:58:03 -0500

    Created POST request to add a car

--- routers/carsRouter.js
+++ routers/carsRouter.js
@@ -29,5 +29,25 @@ router.get('/:id', validateId, async (req, res, next) => {
   };
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    const payload = {
+      vin: req.body.VIN,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmission: req.body.transmission,
+      title: req.body.title
+    }
+    const [id] = await db('cars').insert(payload)
+    res.status(201).json(await db('cars').where('id', id).first())
+  } catch (err) {
+    res.status(500).json({
+      err: err,
+      errorMessage: 'Failed to add car to the database.'
+    })
+    next(err)
+  };
+});
 
 module.exports = router;

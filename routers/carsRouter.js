9eb7a538dedba6d0999d1ab8309bef88d605e5f6
Tara Sherman <taradelin@gmail.com>
2019-12-25 09:57:05 -0500

    Created GET request for all cars

---
+++ routers/carsRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const knex = require('knex');
+const db = require('../data/dbConfig');
+const { validateId } = require('../middleware/validation');
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    res.status(200).json(await db("cars").select())
+  } catch (err) {
+    res.status(500).json({
+      err: err,
+      errorMessage: 'The cars information could not be retrieved.'
+    })
+    next(err)
+  };
+});
+
+
+module.exports = router;
